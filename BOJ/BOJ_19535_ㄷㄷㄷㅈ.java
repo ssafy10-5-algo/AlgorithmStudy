16527d132b186c1ed4f507df04840abb95081d89
da1907 <cd03252@naver.com>
2021-04-24 22:42:19 +0900

    Upload BOJ 19535 ㄷㄷㄷㅈ

--- BOJ/BOJ_19535_ㄷㄷㄷㅈ.java
+++ BOJ/BOJ_19535_ㄷㄷㄷㅈ.java
@@ -1,23 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BOJ_19535_ㄷㄷㄷㅈ {
 
-	private static int N, dCnt, gCnt;
-	private static ArrayList<Integer>[] link;
-	private static Stack<Integer> stack;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		link = new ArrayList[N+1];		// 연결 정보
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer>[] link = new ArrayList[N+1];		// 연결 정보
+		boolean[] visited = new boolean[N+1];
 		for (int i = 0; i < N+1; i++) {
 			link[i] = new ArrayList<Integer>();
 		}
@@ -30,62 +23,34 @@ public class BOJ_19535_ㄷㄷㄷㅈ {
 			link[b].add(a);
 		}
 		
-		for (int i = 1; i < N+1; i++) {
-			//stack = new Stack<Integer>();
-			//stack.add(i);
-			visited = new boolean[N+1];
-			arr[0] = i;
+		long D = 0, G = 0;
+		
+		// ㅈ
+		for (int i = 1; i <= N; i++) {
+			if (link[i].size() >= 3) {
+				long n = link[i].size();
+				G += n * (n-1) * (n-2) / 6;
+			}
+		}
+		
+		// ㄷ
+		for (int i = 1; i <= N; i++) {
+			long child = link[i].size() - 1;
+			
+			for (int j = 0, size=link[i].size(); j < size; j++) {
+				if (visited[link[i].get(j)]) continue;
+				
+				D += child * (link[link[i].get(j)].size() - 1);
+			}
+			
 			visited[i] = true;
-			dfs(i, 1);
 		}
 		
-		if (dCnt > gCnt * 3) System.out.println('D');
-		if (dCnt < gCnt * 3) System.out.println('G');
-		if (dCnt == gCnt * 3) System.out.println("DUDUDUNGA");
-	}
-
-	static int[] arr = new int[4];
-	static boolean[] visited;
-	private static void dfs(int start, int idx) {
-		if (idx == 4) {
-			getLinkCnt();
-			
-			int maxLink = 0;
-			for (int i = 0; i < 4; i++) {
-				maxLink = Math.max(maxLink, count[i]);
-			}
-			
-			if (maxLink == 2)
-				++dCnt;
-			else if (maxLink == 3)
-				++gCnt;
-
-			return;
-		}
 		
-		for (int i = 0, size=link[start].size(); i < size; i++) {
-			int next = link[start].get(i);
-			if (visited[next]) continue;
-			arr[idx] = next;
-			visited[next] = true;
-			dfs(next, idx+1);
-		}
-	}
-	
-	static int[] count = new int[4];
-	private static void getLinkCnt() {
-		for (int i = 0; i < 4; i++) {
-			int cur = arr[i];
-			for (int j = 0, size=link[cur].size(); j < size; j++) {
-				int candidate = link[cur].get(j);
-				for (int k = 0; k < 4; k++) {
-					if (arr[k] == cur) continue;
-					if (arr[k] == candidate) {
-						count[i]++;
-					}
-				}
-			}
-		}
+		if (D > G*3) System.out.println("D");
+		if (D < G*3) System.out.println("G");
+		if (D == G*3) System.out.println("DUDUDUNGA");
+		
 	}
 
 }
